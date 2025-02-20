2cf721f52d140e8b201b6f9ed5042bad58578c28
lealembts <lealemb84@gmail.com>
2025-02-21 01:10:23 +0300

    index

--- react-projects/src/components/accordian/index.jsx
+++ react-projects/src/components/accordian/index.jsx
@@ -7,16 +7,27 @@ import data from "./data";
 export default function Accordian(){
     const [selected, setSelected] = useState(null);
 
-    return <div className="wrapper"> 
-    <div className="accordian">
-        {
-            data && data.length > 0 ? (
-            data.map((dataItem) => (
-            <div className="item">
-                <div className="title">
+    function handleSingleSelection(getCurrentId){
+        // console.log(getCurrentId);
+        setSelected(getCurrentId);
+    }
+
+    console.log(selected);
+    return ( 
+    <div className="wrapper"> 
+        <div className="accordian">
+            {data && data.length > 0 ? (
+                data.map((dataItem) => (
+                 <div className="item">
+                   <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                     <h3>{dataItem.question}</h3>
                     <span>+</span>
                 </div>
+                {
+                    selected === dataItem.id ?
+                    <div className= "content"> {dataItem.answer} </div>
+                    : null
+                }
             </div>)
             ))
             
@@ -26,4 +37,5 @@ export default function Accordian(){
         </div>
 
     </div>
+    )
 }
