a6269f641aed0fd474387ab8ff7d9b4e844d95e7
lealembts <lealemb84@gmail.com>
2025-02-23 18:18:04 +0300

    add multi selection

--- react-projects/src/components/accordian/index.jsx
+++ react-projects/src/components/accordian/index.jsx
@@ -13,15 +13,20 @@ export default function Accordian(){
         setSelected(getCurrentId  ===  selected? null : getCurrentId);
     }
 
+
+    function handleMultiSelection(getCurrentId){
+        
+    }
+
     console.log(selected);
     return (  
     <div className="wrapper"> 
-    <button>Enable Multi Selection</button>
+    <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
         <div className="accordian">
             {data && data.length > 0 ? (
                 data.map((dataItem) => (
                  <div className="item">
-                   <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+                   <div onClick={enableMultiSelection()=> handleMultiSelection(dataItem.id): ()=>handleSingleSelection(dataItem.id)} className="title">
                     <h3>{dataItem.question}</h3>
                     <span>+</span>
                 </div>
