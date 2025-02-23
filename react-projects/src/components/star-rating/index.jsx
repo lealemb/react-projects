435652f1ca7f186d1cff011e1bebae6c9862f3b8
lealembts <lealemb84@gmail.com>
2025-02-23 23:24:43 +0300

    file added

--- react-projects/src/components/star-rating/index.jsx
+++ react-projects/src/components/star-rating/index.jsx
@@ -2,17 +2,34 @@
 import {FaStar}  from 'react-icons/fa'
 
 
-export default function StarRating({ noOfStars }) {
+export default function StarRating({ noOfStars = 5 }) {
 
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex){
+
+
+    }
+
+    function handleMouseEnter(){
+
+
+    }
+
+    function handleMouseLeave(){
+
+    }
 return <div className="star-rating"> 
           {
             [ ...Array(noOfStars)].map((_, index) => {
+                index += 1;
 
                 return <FaStar 
                 key={index}
-                onClick={}
-                onMouseMove={}
-                onMouseLeave={}
+                onClick={()=> handleClick(index)}
+                onMouseMove={()=> handleMouseEnter(index)}
+                onMouseLeave={ ()=> handleMouseLeave(index)}
                 size={40}
                 />
             })
